6b4da54b989eaabf5eb8b6d54ef0525b03fc7b46
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-10-01 21:45:06 -0400

    DayListItem Component for App

---
+++ src/components/DayListItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  let text = "";
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      text = "no spots remaining";
+    } else if (props.spots === 1) {
+      text = `${props.spots} spot remaining`;
+    } else {
+      text = `${props.spots} spots remaining`;
+    }
+    return text;
+  };
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  )
+}
+
+export default DayListItem;
