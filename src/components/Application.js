427c513a00774ebd66606b3116213db32816a1cd
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-10-02 00:47:09 -0400

    Request the days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
 
 import "components/Application.scss";
 
@@ -6,24 +8,6 @@ import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 
 /***Sample Data ***/
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 const appointments = [
   {
     id: 1,
@@ -73,6 +57,14 @@ const appointments = [
 
 function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days")
+      .then(response => {
+        setDays([...response.data]);
+      });
+  }, []);
 
   /****Iterating over Appointments */
   const appointmentList = appointments.map(appointment =>
