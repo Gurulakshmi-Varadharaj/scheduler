4f140f580a387c599191bc88cf77451007b794df
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-10-04 00:36:39 -0400

    Use custom Hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,12 +6,35 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form"
+
+import { useVisualMode } from "hooks/useVisualMode";
 
 function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  let { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+
+  //Render the component based on the custom Hook retun value
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {!props.interview ? <Empty /> : <Show student={props.interview.student} interviewer={props.interview.interviewer} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onSave={props.onSave}
+          onCancel={() => back()}
+        />
+      )}
     </article>
   );
 }
